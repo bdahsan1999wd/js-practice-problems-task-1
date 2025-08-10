c285951f6bd1a4a29b53384e7c98493186db9012
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-10 23:57:53 +0600

    task-1 added

---
+++ task-1.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(celsius) {
+    // Formula: (°C × 9/5) + 32 = °F
+    return (celsius * 9 / 5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(25));
+console.log(celsiusToFahrenheit(100));
